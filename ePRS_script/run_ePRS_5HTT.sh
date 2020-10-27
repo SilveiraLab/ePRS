501e88482dacfc6f36ec87468f9dedd387c0007c
natacha-beck <natacha.beck@mcgill.ca>
2020-10-27 00:06:41 -0400

    [FIX] run_ePRS_5HTT.sh script

--- ePRS_script/run_ePRS_5HTT.sh
+++ ePRS_script/run_ePRS_5HTT.sh
@@ -3,61 +3,80 @@ set -e
 
 ### MEANEY LAB ###
 
-mkdir ePRS_score
+GEN_FILE=$1
+SAMPLE_FILE=$2
+OUTPUT=$3
 
-GENO_data_file=$1
 
-#NEED TO ENTER::
+# Copy INPUT in OUTPUT dir (the script writting in the INPUT directory)
+mkdir -p $OUTPUT
+DIR_TO_CP_INPUTS=$OUTPUT/input_data
+mkdir           $DIR_TO_CP_INPUTS
+cp $GEN_FILE    $DIR_TO_CP_INPUTS
+cp $SAMPLE_FILE $DIR_TO_CP_INPUTS
+
+GEN_FILE_BASENAME=`basename $GEN_FILE`
+echo "GEN_FILE_BASENAME"
+echo $GEN_FILE_BASENAME
+GEN_FILE_BASENAME_WITHOUT_EXT="${GEN_FILE_BASENAME%.*}"
+echo "GEN_FILE_BASENAME_WITHOUT_EXT"
+echo $GEN_FILE_BASENAME_WITHOUT_EXT
+GEN_FILE_WITHOUT_EXT=$DIR_TO_CP_INPUTS/$GEN_FILE_BASENAME_WITHOUT_EXT
+echo "GEN_FILE_WITHOUT_EXT"
+echo $GEN_FILE_WITHOUT_EXT
+
 
 GWAS="/ePRS/fix_data/gwas_nodup_noambig.txt"
 PRSOS="/PRSoS/PRSoS.py"
 
+### RUN ###
+
 echo subset geno + GWAS
 
-awk 'NR==FNR{a[$1];next}$2 in a{print}' $GWAS $GENO_data_file.gen       > ePRS_score/score.gentemp
-head -1 $GWAS                                                           > ePRS_score/score_gwastemp.txt
-awk 'NR==FNR{a[$2];next}$1 in a{print}' ePRS_score/score.gentemp $GWAS >> ePRS_score/score_gwastemp.txt
-cut -f1-8 -d' ' ePRS_score/score.gentemp                                > ePRS_score/score.gentempnosam
+awk 'NR==FNR{a[$1];next}$2 in a{print}' $GWAS $GEN_FILE              > $OUTPUT/score.gentemp
+head -1 $GWAS                                                        > $OUTPUT/score_gwastemp.txt
+awk 'NR==FNR{a[$2];next}$1 in a{print}' $OUTPUT/score.gentemp $GWAS >> $OUTPUT/score_gwastemp.txt
+cut -f1-8 -d' ' $OUTPUT/score.gentemp                                > $OUTPUT/score.gentempnosam
 
 echo output clumpit file
-cut -f1-2 ePRS_score/score_gwastemp.txt                                >> ePRS_score/score_clumpit.txt
+cut -f1-2 $OUTPUT/score_gwastemp.txt                                >> $OUTPUT/score_clumpit.txt
 
-if [ -e $GENO_data_file.fam ]; then
+if [ -e $GEN_FILE.fam ]; then
   echo "bfile exists"
 else
   echo "using plink to convert gen to bfile"
-  plink --gen $GENO_data_file.gen \
-        --sample $GENO_data_file.sample \
+  plink --gen $GEN_FILE \
+        --sample $SAMPLE_FILE \
         --keep-allele-order \
         --make-bed \
-        --out $GENO_data_file
+        --out $GEN_FILE_WITHOUT_EXT
 fi
 
 echo "Clumping"
 
-plink --bfile $GENO_data_file \
-      --clump ePRS_score/score_clumpit.txt \
+plink --bfile $GEN_FILE_WITHOUT_EXT \
+      --clump $OUTPUT/score_clumpit.txt \
       --clump-p1 1 --clump-p2 1 --clump-kb 500 --clump-r2 0.2 \
-      --out ePRS_score/score_clumpit
+      --out $OUTPUT/score_clumpit
 
-awk 'NR==FNR{a[$3];next}$2 in a{print}' ePRS_score/score_clumpit.clumped ePRS_score/score.gentemp           > ePRS_score/score_clumped1.gen
-awk 'NR==FNR{a[$2];next}$1 in a{print}' ePRS_score/score_clumped1.gen ePRS_score/score_gwastemp.txt        >> ePRS_score/score_clumped1.txt
-cat /ePRS/fix_data/header.txt                                                                               > ePRS_score/score_clumped.txt
-awk '{$7=$6"-"$1} 1' ePRS_score/score_clumped1.txt | awk 'BEGIN {FS=" ";OFS="\t"} {$1=$1}1'                >> ePRS_score/score_clumped.txt
-awk 'NR==FNR{a[$1]=$7; next} $2 in a{$2=a[$2]}1' ePRS_score/score_clumped.txt ePRS_score/score_clumped1.gen > ePRS_score/score_clumped2.gen
-cat /ePRS/fix_data/header2.txt                                                                              > ePRS_score/score_clumped2.txt
-awk 'NR>1{print $7, $2, $3, $4, $5}' ePRS_score/score_clumped.txt | awk 'BEGIN {FS=" ";OFS="\t"} {$1=$1}1' >> ePRS_score/score_clumped2.txt
+awk 'NR==FNR{a[$3];next}$2 in a{print}' $OUTPUT/score_clumpit.clumped $OUTPUT/score.gentemp              > $OUTPUT/score_clumped1.gen
+awk 'NR==FNR{a[$2];next}$1 in a{print}' $OUTPUT/score_clumped1.gen $OUTPUT/score_gwastemp.txt           >> $OUTPUT/score_clumped1.txt
+cat /ePRS/fix_data/header.txt                                                                            > $OUTPUT/score_clumped.txt
+awk '{$7=$6"-"$1} 1' $OUTPUT/score_clumped1.txt | awk 'BEGIN {FS=" ";OFS="\t"} {$1=$1}1'                >> $OUTPUT/score_clumped.txt
+awk 'NR==FNR{a[$1]=$7; next} $2 in a{$2=a[$2]}1' $OUTPUT/score_clumped.txt $OUTPUT/score_clumped1.gen    > $OUTPUT/score_clumped2.gen
+cat /ePRS/fix_data/header2.txt                                                                           > $OUTPUT/score_clumped2.txt
+awk 'NR>1{print $7, $2, $3, $4, $5}' $OUTPUT/score_clumped.txt | awk 'BEGIN {FS=" ";OFS="\t"} {$1=$1}1' >> $OUTPUT/score_clumped2.txt
 
 echo "ePRS calculation"
 # add parameters accordingly::
-spark-submit --master local[12] $PRSOS ePRS_score/score_clumped2.gen ePRS_score/score_clumped2.txt ePRS_score/score_prs \
-             --sample $GENO_data_file.sample \
+spark-submit --master local[12] $PRSOS $OUTPUT/score_clumped2.gen $OUTPUT/score_clumped2.txt $OUTPUT/score_prs \
+             --sample $SAMPLE_FILE \
              --snp_log \
              --filetype GEN \
              --no_a1f \
              --thresholds 1 \
              --check_dup
 
-cd ePRS_score
+cd $OUTPUT
 ls | grep clump | xargs rm
 ls | grep temp | xargs rm
